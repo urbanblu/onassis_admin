d3215550aba2279a7d3e0a4f49796b9be40bf359
samuel <s.twumasi@movanow.co>
2026-04-09 10:08:48 +0000

    Implement client-side authentication check in DashboardLayout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import GlobalNavbar from "@/components/global-navbar";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import useAuth from "@/stores/auth.store";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const { auth } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!auth) {
+      router.replace("/login");
+    }
+  }, [auth, router]);
+
   return <GlobalNavbar>{children}</GlobalNavbar>;
 }
